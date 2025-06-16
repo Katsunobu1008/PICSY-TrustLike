651b958a5f7e0e6cc64aa3f8c1c1044a9430fcd1
Katsunobu1008 <katsunobu1008@gmail.com>
2025-06-16 23:20:59 +0900

    Content のデータモデル定義と User モデルの更新

---
+++ app/models/content.py
@@ -0,0 +1,26 @@
+# app/models/content.py
+
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+from ..database import Base
+
+
+class Content(Base):
+    __tablename__ = "contents"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True, nullable=False)
+    body = Column(Text, nullable=True)  # 本文は長くなる可能性があるのでText型
+
+    # 外部キー制約: usersテーブルのidカラムと関連付ける
+    creator_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True),
+                        server_default=func.now(), onupdate=func.now())
+
+    # Userモデルとのリレーションシップ（関連付け）を定義
+    # "creator"という名前で、このContentオブジェクトから対応するUserオブジェクトにアクセスできるようになる
+    creator = relationship("User", back_populates="contents")
