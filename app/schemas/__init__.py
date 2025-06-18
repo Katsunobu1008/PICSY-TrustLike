78f1d3011b8f05f719066fc7646ce1811211b504
Katsunobu1008 <katsunobu1008@gmail.com>
2025-06-18 15:25:50 +0900

    schemas ディレクトリをPythonパッケージとして機能させ、内部のスキーマクラスを外部からインポートしやすく

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,5 +1,5 @@
-# app/schemas/__init__.py (追記)
+# app/schemas/__init__.py
 
-from .user import User, UserCreate  # UserInDBBaseは内部的なので、公開しなくても良い
+from .user import User, UserCreate
 from .token import Token, TokenData
-from .content import Content, ContentCreate  # Content関連スキーマを追加
+from .content import Content, ContentCreate
